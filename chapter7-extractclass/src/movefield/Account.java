fc0c7929c9443cd7672a73f9df1baddfe466e472
Huamao.zhang <huamao.zhang@ck-telecom.com>
2018-12-18 13:21:44 +0800

    extract class-1

---
+++ chapter7-extractclass/src/movefield/Account.java
@@ -0,0 +1,29 @@
+package movefield;
+
+public class Account {
+    private AccountType mType;
+    // step0. 如果有多个函数都使用到了mInterestRate字段，需要先运用self encapsulate进行自我封装
+    // private double mInterestRate;
+
+    public Account(AccountType mType) {
+        this.mType = mType;
+    }
+
+    double interestForAmountDays(double amount, int days) {
+        return  getInterestRate() * amount * days /365;
+    }
+
+    double interestForAmountYears(double amount, int years) {
+        return  getInterestRate() * amount * years;
+    }
+
+    // step2. 替换字段
+    private void setInterestRate(double value) {
+        mType.setInterestRate(value);
+    }
+
+    // step2. 替换字段
+    private double getInterestRate() {
+        return mType.getInterestRate();
+    }
+}
---
+++ chapter7-extractclass/src/movefield/AccountTest.java
@@ -0,0 +1,26 @@
+package movefield;
+
+import junit.framework.TestCase;
+
+public class AccountTest extends TestCase {
+    private Account mAccount;
+    private AccountType mType;
+
+    public void setUp() throws Exception {
+        // step4. 修改测试用例
+        mType = new AccountType();
+        mAccount = new Account(mType);
+        mType.setInterestRate(1.5);
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testInterestForAmountDays() {
+        System.out.println("testInterestForAmountDays : " + mAccount.interestForAmountDays(2, 365));
+    }
+
+    public void  testInterestForAmountYears() {
+        System.out.println("testInterestForAmountYears : " + mAccount.interestForAmountYears(2, 1));
+    }
+}
